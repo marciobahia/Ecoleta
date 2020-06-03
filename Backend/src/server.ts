085b602709eb6c7a2b8a4e27860c7ecf50d3420b
marciobahia <marciosellabahia@hotmail.com>
2020-06-03 09:31:10 -0300

    Backend Finalizado

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,24 +1,13 @@
-import express, { response } from 'express';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
+app.use(cors())
+app.use(express.json());  
+app.use(routes);
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de Usuários');
-
-    response.json([
-        'Diego',
-        'Márcio',
-        'Robson',
-        'Atílio'
-    ]);
-})
-
-
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
-
-
-
-
-
-
